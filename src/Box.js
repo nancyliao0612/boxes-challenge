7deb49986e252e2b1f6c71733e347d8512b96571
Nancy <nancy62924@gmail.com>
2022-02-09 11:19:36 +0800

    flip boxes' colors

---
+++ src/Box.js
@@ -0,0 +1,7 @@
+export default function Box(props) {
+  const styles = {
+    backgroundColor: props.on ? "#222222" : "transparent",
+  };
+
+  return <div onClick={props.toggle} style={styles} className="box"></div>;
+}
